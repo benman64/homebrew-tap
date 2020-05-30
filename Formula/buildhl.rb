cf371b6b870175c4ce78f58ef8eaf11a24655d24
Ben Manashirov <benzakhar@gmail.com>
2020-05-30 17:58:47 -0400

    audit passes

--- Formula/buildhl.rb
+++ Formula/buildhl.rb
@@ -1,5 +1,5 @@
 class Buildhl < Formula
-  desc "Tool that does syntax highlighting and file rewrites of builds in your terminal"
+  desc "Syntax highlighting and path rewrites of build output in your terminal"
   homepage "https://github.com/benman64/buildhl"
   url "https://github.com/benman64/buildhl/archive/v0.2.0.tar.gz"
   sha256 "d7a9be0ec92158d35010c2bd7a31a0cbd4fd8da8bd364fffe9d86a300450abdd"
